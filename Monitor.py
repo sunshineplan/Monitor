2a6bb1a518b054cc895d189e2ad3f3031421e0db
sunshineplan <sunshineplan@gmail.com>
2019-07-18 19:58:08 +0800

    Separate income and limit

--- Monitor.py
+++ Monitor.py
@@ -22,47 +22,72 @@ logger.addHandler(handler)
 
 class Monitor:
     def __init__(self):
+        self.nbcb_1004_income = '-'
+        self.nbcb_1004_limit = None
+        self.nbcb_1005_income = '-'
+        self.nbcb_1005_limit = None
         self.spdb_flag = 0
-        self.nbcb_1004 = None
-        self.nbcb_1005 = None
-        self.spdb_2301187111 = None
+        self.spdb_2301187111_income = '-'
+        self.spdb_2301187111_limit = None
         self.parserconfig()
 
     def parserconfig(self):
         config = configparser.ConfigParser()
         config.read(os.path.join(os.path.abspath(
             os.path.dirname(__file__)), 'Monitor.ini'))
+        self.timeout = config.getint('DEFAULT', 'timeout', fallback=5)
         self.log_interval = config.getint(
             'DEFAULT', 'log_interval', fallback=15)
         self.config_interval = config.getint(
             'DEFAULT', 'config_interval', fallback=150)
+        self.income_interval = config.getint(
+            'DEFAULT', 'income_interval', fallback=3600)
         self.agent = config.get('DEFAULT', 'agent', fallback='Monitor')
         self.nbcb_interval = config.getint('nbcb', 'interval', fallback=7)
         self.spdb_interval = config.getint('spdb', 'interval', fallback=1)
         self.spdb_cookies = config.get('spdb', 'cookies', fallback='')
 
+    async def income(self):
+        headers = {'User-Agent': self.agent}
+        data = {'prodId': 1004, 'replyPage': 1}
+        try:
+            self.nbcb_1004_income = BeautifulSoup(requests.post('https://mybank.nbcb.com.cn/doorbank/queryNav.do',
+                                                                params=data, headers=headers).text, 'html.parser').find('td', class_='td4').text
+        except:
+            self.nbcb_1004_income = '-'
+        data = {'prodId': 1005, 'replyPage': 1}
+        try:
+            self.nbcb_1005_income = BeautifulSoup(requests.post('https://mybank.nbcb.com.cn/doorbank/queryNav.do',
+                                                                params=data, headers=headers).text, 'html.parser').find('td', class_='td4').text
+        except:
+            self.nbcb_1005_income = '-'
+        headers = {'Cookie': self.spdb_cookies}
+        data = {'FinanceNo': '2301187111',
+                'nearDate': 'nearOneWeek', 'Hierarchy': '2301187111-A'}
+        try:
+            self.spdb_2301187111_income = loads(requests.post('https://ebank.spdb.com.cn/msper-web-finance/QueryFinanceIncomeByAjax.json',
+                                                              json=data, headers=headers).json()['dataList'])[-1]
+        except:
+            self.spdb_2301187111_income = '-'
+        await asyncio.sleep(self.income_interval)
+        asyncio.ensure_future(self.income())
+
     async def nbcb(self):
         try:
-            headers = {'user-agent': self.agent}
-            data = {'prodId': 1004, 'replyPage': 1}
-            income_1004 = BeautifulSoup(requests.post('https://mybank.nbcb.com.cn/doorbank/queryNav.do',
-                                                      params=data, headers=headers).text, 'html.parser').find('td', class_='td4').text
-            data = {'prodId': 1005, 'replyPage': 1}
-            income_1005 = BeautifulSoup(requests.post('https://mybank.nbcb.com.cn/doorbank/queryNav.do',
-                                                      params=data, headers=headers).text, 'html.parser').find('td', class_='td4').text
+            headers = {'User-Agent': self.agent}
             data = {'riskLevel': 2, 'isSelling': 1, 'orderType': 2,
                     'investTerm': 4, 'TemplateCode': 9901, 'WEB_CHN': 'NH'}
             limit = requests.post('https://e.nbcb.com.cn/perbank/HB06201_noSessionFinancialProds.do',
-                                  params=data, headers=headers).json()['cd']['iProdUseLimits']
+                                  params=data, headers=headers, timeout=self.timeout).json()['cd']['iProdUseLimits']
             for i in limit:
                 if i['prodId'] == '1004':
-                    self.nbcb_1004 = self.markup(
-                        self.nbcb_1004, (f'天利鑫B({income_1004})', i['totUseLimit']))
+                    self.nbcb_1004_limit = self.markup(
+                        self.nbcb_1004_limit, (f'天利鑫B({self.nbcb_1004_income})', i['totUseLimit']))
                 if i['prodId'] == '1005':
-                    self.nbcb_1005 = self.markup(
-                        self.nbcb_1005, (f'如意鑫A({income_1005})', i['totUseLimit']))
+                    self.nbcb_1005_limit = self.markup(
+                        self.nbcb_1005_limit, (f'如意鑫A({self.nbcb_1005_income})', i['totUseLimit']))
         except:
-            self.nbcb_1004 = self.nbcb_1005 = None
+            self.nbcb_1004_limit = self.nbcb_1005_limit = None
         await asyncio.sleep(self.nbcb_interval)
         asyncio.ensure_future(self.nbcb())
 
@@ -71,15 +96,18 @@ class Monitor:
             headers = {'Cookie': self.spdb_cookies}
             data = {'FinanceNo': '2301187111',
                     'nearDate': 'nearOneWeek', 'Hierarchy': '2301187111-A'}
-            income = loads(requests.post('https://ebank.spdb.com.cn/msper-web-finance/QueryFinanceIncomeByAjax.json',
-                                         json=data, headers=headers).json()['dataList'])[-1]
-            limit = requests.post('https://ebank.spdb.com.cn/msper-web-finance/QueryAvlLimitAmnt.json',
-                                  json=data, headers=headers).json()['CanUseQuota'].replace(',', '')
-            if not test:
-                self.spdb_2301187111 = self.markup(
-                    self.spdb_2301187111, (f'天添盈增利1号({income}%)', limit))
-                await asyncio.sleep(self.spdb_interval)
-                asyncio.ensure_future(self.spdb())
+            try:
+                limit = requests.post('https://ebank.spdb.com.cn/msper-web-finance/QueryAvlLimitAmnt.json',
+                                    json=data, headers=headers, timeout=self.timeout).json()['CanUseQuota'].replace(',', '')
+                if not test:
+                    self.spdb_2301187111_limit = self.markup(
+                        self.spdb_2301187111_limit, (f'天添盈增利1号({self.spdb_2301187111_income}%)', limit))
+                    await asyncio.sleep(self.spdb_interval)
+                    asyncio.ensure_future(self.spdb())
+            except:
+                self.spdb_flag = 0
+                self.spdb_2301187111_limit = None
+                raise ValueError
 
     async def spdb_test(self):
         try:
@@ -88,8 +116,7 @@ class Monitor:
                 asyncio.ensure_future(self.spdb())
             self.spdb_flag = 1
         except:
-            self.spdb_flag = 0
-            self.spdb_2301187111 = None
+            pass
         await asyncio.sleep(self.config_interval)
         asyncio.ensure_future(self.spdb_test())
 
@@ -106,7 +133,8 @@ class Monitor:
             return (value[0], value[1]+'↑')
 
     def formatter(self, with_mark=True):
-        products = [self.nbcb_1004, self.nbcb_1005, self.spdb_2301187111]
+        products = [self.nbcb_1004_limit,
+                    self.nbcb_1005_limit, self.spdb_2301187111_limit]
         products = [i for i in products if i]
         contents = []
         for i in products:
@@ -137,6 +165,7 @@ class Monitor:
         asyncio.ensure_future(self.display())
 
     async def main(self):
+        asyncio.ensure_future(self.income())
         asyncio.ensure_future(self.nbcb())
         asyncio.ensure_future(self.spdb_test())
         asyncio.ensure_future(self.record())
